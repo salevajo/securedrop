7220f464e0c4c6047d4efef09e15c3590703f215
Kunal Mehta <legoktm@debian.org>
2024-10-28 14:12:21 -0400

    Remove test_securedrop_application_apt_dependencies test
    We have much more confidence in our Debian packages now, so we don't
    need to keep a duplicated list of dependencies and check that they're
    installed.
    
    This happened to be the last use of the `python_version` variable, so
    remove that entirely.

--- molecule/testinfra/app-code/test_securedrop_app_code.py
+++ molecule/testinfra/app-code/test_securedrop_app_code.py
@@ -3,7 +3,6 @@ import testutils
 
 securedrop_test_vars = testutils.securedrop_test_vars
 testinfra_hosts = [securedrop_test_vars.app_hostname]
-python_version = securedrop_test_vars.python_version
 
 
 def test_apache_default_docroot_is_absent(host):
@@ -13,32 +12,6 @@ def test_apache_default_docroot_is_absent(host):
     leak, as it displays version information by default.
     """
     assert not host.file("/var/www/html").exists
-
-
-@pytest.mark.parametrize(
-    "package",
-    [
-        "apache2",
-        "apparmor-utils",
-        "coreutils",
-        "gnupg2",
-        "libapache2-mod-xsendfile",
-        f"libpython{python_version}",
-        "paxctld",
-        "python3",
-        "redis-server",
-        "securedrop-config",
-        "securedrop-keyring",
-        "sqlite3",
-    ],
-)
-def test_securedrop_application_apt_dependencies(host, package):
-    """
-    Ensure apt dependencies required to install `securedrop-app-code`
-    are present. These should be pulled in automatically via apt,
-    due to specification in Depends in package control file.
-    """
-    assert host.package(package).is_installed
 
 
 @pytest.mark.parametrize(
