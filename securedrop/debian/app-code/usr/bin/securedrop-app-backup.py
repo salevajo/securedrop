81a6727a753a21be20083ffb73462dae11bd0ba9
Kunal Mehta <legoktm@debian.org>
2024-11-07 17:23:50 -0500

    Give better control over where backups are stored
    Instead of saving them in the current directory and requiring awkward
    workarounds to change that directory, take it as an argument so the
    script can be run from anywhere without requiring context changes.

--- securedrop/debian/app-code/usr/bin/securedrop-app-backup.py
+++ securedrop/debian/app-code/usr/bin/securedrop-app-backup.py
@@ -8,12 +8,31 @@ Admin Workstation by the playbook, and removed on the server. For further
 information and limitations, see https://docs.securedrop.org/en/stable/backup_and_restore.html
 """
 
+import argparse
 import os
+import sys
 import tarfile
 from datetime import datetime
+from pathlib import Path
 
 
-def main():
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Create backup")
+    parser.add_argument(
+        "--dest",
+        type=Path,
+        default=os.getcwd(),
+        help="Destination folder for backup",
+    )
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    if not args.dest.exists():
+        print(f"Error: {args.dest} does not exist")
+        sys.exit(1)
+
     backup_filename = "sd-backup-{}.tar.gz".format(datetime.utcnow().strftime("%Y-%m-%d--%H-%M-%S"))
 
     # This code assumes everything is in the default locations.
@@ -26,7 +45,7 @@ def main():
     tor_hidden_services = "/var/lib/tor/services"
     torrc = "/etc/tor/torrc"
 
-    with tarfile.open(backup_filename, "w:gz") as backup:
+    with tarfile.open(os.path.join(args.dest, backup_filename), "w:gz") as backup:
         backup.add(sd_config)
 
         # If no custom logo has been configured, the file will not exist
